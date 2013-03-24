d4c261e658dcc4ae1a8e784c04ad8a33f9a3dcf5
myfreeweb <floatboth@me.com>
2013-03-24 15:21:52 +0400

    cleanup adn.rb

--- app/adn.rb
+++ app/adn.rb
@@ -3,13 +3,8 @@ require 'em-synchrony'
 require 'faraday'
 require 'faraday_middleware'
 require 'faraday_middleware/multi_json'
-require_relative 'const'
 
 class ADN
-  class << self
-    attr_accessor :global
-  end
-
   def initialize(token)
     @api = Faraday.new(:url => 'https://alpha-api.app.net/stream/0/') do |adn|
       adn.request  :authorization, 'Bearer', token
